443cf790cdb5c2f9100ea126ddb78ee60f4b5680
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-06-30 15:01:29 -0300

    feat: gerencia search e selectedHero no App, passa para páginas via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import HeroDetails from "./pages/HeroDetails";
+import useHeros from "./hooks/useHeros";
+import type { HeroType } from "./types/HeroType";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+  const [selectedHero, setSelectedHero] = useState<HeroType | null>(null);
+
+  const { heros = [] } = useHeros();
+
   return (
     <BrowserRouter>
       <div className="text-textSecondary font-display flex min-h-screen flex-col">
         <div className="flex-1">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/hero" element={<HeroDetails />} />
+            <Route
+              path="/"
+              element={
+                <Home
+                  search={search}
+                  setSearch={setSearch}
+                  setSelectedHero={setSelectedHero}
+                  heros={heros}
+                />
+              }
+            />
+            <Route
+              path="/hero"
+              element={
+                <HeroDetails
+                  search={search}
+                  setSearch={setSearch}
+                  hero={selectedHero}
+                />
+              }
+            />
           </Routes>
         </div>
         <footer className="bg-primary h-[5vh] w-full"></footer>
