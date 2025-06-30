7175778a25dd4d1da9576b645e91eb35ef191544
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-06-30 17:30:31 -0300

    feat: adiciona funcionalidade de favoritar herói no HeroCard

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -2,15 +2,17 @@ type HeroCardProps = {
   name: string;
   image: string;
   onFavoriteClick?: () => void;
+  isFavorited?: boolean;
 };
 
 export default function HeroCard({
   name,
   image,
+  isFavorited,
   onFavoriteClick,
 }: HeroCardProps) {
   return (
-    <div className="mb-1.5 flex flex-col gap-2">
+    <div className="mb-1.5 flex cursor-pointer flex-col gap-2">
       <div className="border-b-primary h-24 w-full overflow-hidden border-b-2">
         <img className="h-full w-full object-cover" src={image} alt={name} />
       </div>
@@ -18,10 +20,17 @@ export default function HeroCard({
       <div className="text-textPrimary flex items-center justify-between text-[8px] font-semibold">
         <span>{name}</span>
         <img
-          src="/icones/heart/Path Copy 2@1,5x.svg"
+          src={
+            isFavorited
+              ? "/icones/heart/Path@3x.png"
+              : "/icones/heart/Path Copy 2@1,5x.svg"
+          }
           alt="Favoritar"
           className="h-2 w-2 cursor-pointer"
-          onClick={onFavoriteClick}
+          onClick={(e) => {
+            e.stopPropagation();
+            onFavoriteClick?.();
+          }}
         />
       </div>
     </div>
