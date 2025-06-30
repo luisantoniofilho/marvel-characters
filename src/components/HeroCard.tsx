7503860e1743fa46574c0d8592b586cea581f466
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-06-30 14:43:05 -0300

    feat: cria componente HeroCard para exibição de heróis

---
+++ src/components/HeroCard.tsx
@@ -0,0 +1,29 @@
+type HeroCardProps = {
+  name: string;
+  image: string;
+  onFavoriteClick?: () => void;
+};
+
+export default function HeroCard({
+  name,
+  image,
+  onFavoriteClick,
+}: HeroCardProps) {
+  return (
+    <div className="mb-1.5 flex flex-col gap-2">
+      <div className="border-b-primary h-24 w-full overflow-hidden border-b-2">
+        <img className="h-full w-full object-cover" src={image} alt={name} />
+      </div>
+
+      <div className="text-textPrimary flex items-center justify-between text-[8px] font-semibold">
+        <span>{name}</span>
+        <img
+          src="/icones/heart/Path Copy 2@1,5x.svg"
+          alt="Favoritar"
+          className="h-2 w-2 cursor-pointer"
+          onClick={onFavoriteClick}
+        />
+      </div>
+    </div>
+  );
+}
